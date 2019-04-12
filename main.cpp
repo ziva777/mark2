5a73295c1975b9afc14c05b34f1d9d4aa56379d7
ziva777 <ziva777@gmail.com>
2019-04-12 15:39:39 +0300

    Изменения, которые будут включены в коммит: 	изменено:      CMakeLists.txt 	изменено:      main.cpp
    + App class to wrap business logic
    + train model from urls

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
+#include <list>
 
 #include "misc.h"
+#include "pipe_read.h"
 #include "str_filter.h"
 #include "markov_model.h"
 #include "markov_model_tutor.h"
@@ -38,40 +41,263 @@ std::string data_src2 =
     "[fish,two]  : [{fish:1}, 1]\n"
     "";
 
+void
+mode1(size_t order)
+{
+    /* Create model and serialize it into cout */
+}
 
-#include <vector>
-#include <list>
+void
+mode2()
+{
+    /* Load model from file and execute generation */
+}
 
-#include "pipe_read.h"
+/*
+mode1 - ./mark2 c <order>
+mode2 - ./mark2 e <input_model.txt> 
+*/
 
+void 
+mode_unknown()
+{
 
+}
 
+class App {
+public:
+    using Args = std::list<std::string>;
 
+    App(Args &&args);
+    int exec();
 
+private:
+    static const char MODE_CREATE_MODEL[];
+    static const char MODE_EXEC_MODEL[];
+    static const char LOCALE[];
 
+    using Urls = std::list<std::string>;
 
+    Args args_;
 
+    int create_mode_(const std::string &arg);
+    int exec_mode_(const std::string &arg);
+    int unknown_mode_();
 
+    void invalid_args_msg_();
 
+    MarkovModel create_model_from_urls_(
+            size_t order, 
+            Urls &&urls);
+};
 
-//////////////////////////////////////
+const char App::MODE_CREATE_MODEL[] = "c";
+const char App::MODE_EXEC_MODEL[] = "e";
+const char App::LOCALE[] = "ru_RU.UTF-8";
+
+App::App(
+        Args &&args)
+: args_{std::move(args)}
+{
+}
 
 int
-main()
+App::exec()
 {
-    std::list<std::string> files = {
-        {"file1"},
-        {"file2"},
-    };
-
-    for (int i = 0; i != 100; ++i) {
-        PipeRead pipe;
-        std::string cmd =
-            "curl -s file:///home/marina/programming/git/mark2/case_003.txt";
-        auto res = pipe.read(cmd);
-        // std::cout << "out = " << res.first << std::endl;
-        std::cout << "ok  = " << res.second << std::endl;
+    if (args_.size() != 2) {
+        invalid_args_msg_();
+        return EXIT_FAILURE;
     }
+
+    auto itr = args_.begin();
+    const auto &arg1 = *itr++;
+    const auto &arg2 = *itr++;
+    int result_code;
+
+    if (arg1 == MODE_CREATE_MODEL) {
+        result_code = create_mode_(arg2);
+    } else if (arg1 == MODE_EXEC_MODEL) {
+        result_code = exec_mode_(arg2);
+    } else {
+        result_code = unknown_mode_();
+    }
+
+    return result_code;
+}
+
+int 
+App::create_mode_(const std::string &arg)
+{
+    try {
+        size_t order = std::stoll(arg);
+        Urls urls;
+
+        for (std::string url; std::getline(std::cin, url);) {
+            if (!url.empty())
+                urls.emplace_back(std::move(url));
+        }
+
+        if (urls.empty()) {
+            std::cout << "OOPS! The URLs list are empty!" << std::endl;
+            return EXIT_FAILURE;
+        }
+        
+        MarkovModel model = create_model_from_urls_(order, std::move(urls));
+        MarkovModelSerializer serializer;
+
+        serializer.to_stream(model, std::cout);
+
+        return EXIT_SUCCESS;
+
+    } catch (const std::invalid_argument &e) {
+        std::cerr << "OOPS! " << e.what() << std::endl;
+    } catch (const std::out_of_range &e) {
+        std::cerr << "OOPS! " << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "OOPS! unknown error"<< std::endl;
+    }
+
+    return EXIT_FAILURE;
+}
+
+int 
+App::exec_mode_(const std::string &arg)
+{
+
+}
+
+int 
+App::unknown_mode_()
+{
+    invalid_args_msg_();
+    return EXIT_FAILURE;
+}
+
+void 
+App::invalid_args_msg_()
+{
+    std::cout << "Invalid args!\n";
+    std::cout << "To build model use: ./mark2 " 
+              << MODE_CREATE_MODEL << " <order>\n";
+    std::cout << "To run model use  : ./mark2 "
+              << MODE_EXEC_MODEL << " <model_file>";
+    std::cout << std::endl;
+}
+
+MarkovModel 
+App::create_model_from_urls_(
+        size_t order, 
+        Urls &&urls)
+{
+    MarkovModel model(order);
+    MarkovModelTutor tutor;
+    StrFilter filter;
+    PipeRead pipe;
+
+    for (auto &url: urls) {
+        std::string cmd = "curl -s " + url;
+        auto res = pipe.read(cmd);
+
+        if (res.second) {
+            if (!res.first.empty()) {
+                filter.process(res.first, LOCALE);
+                tutor.train(model, std::move(res.first), false);
+            }
+        } else {
+            std::cerr << "OOPS! Can't process file ";
+            std::cerr << url << std::endl;
+        }
+    }
+
+    model.calc_weights();
+
+    return model;
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    std::list<std::string> args;
+
+    for (int i = 1; i != argc; ++i) {
+        args.emplace_back(argv[i]);
+    }
+
+    App app(std::move(args));
+    return app.exec();
+
+    // if (argc == 3) {
+    //     App app(
+    //         std::list<std::string>{
+    //             {argv[1]}, {argv[2]}
+    //         }
+    //     );
+
+    //     return app.exec();
+    //     // std::string mode{argv[1]};
+    //     // std::string arg{argv[2]};
+
+    //     // switch (mode) {
+    //     //     case "c":
+    //     //         mode1();
+    //     //         break;
+    //     //     case "e":
+    //     //         mode2();
+    //     //         break;
+    //     //     default:
+    //     //         mode_unknown();
+    //     //         return EXIT_FAILED;
+    //     //         break;
+    //     // }
+    // } else {
+    //     std::cout << "Invalid args!\n";
+    //     std::cout << "To build model use: ./mark2 c <order>\n";
+    //     std::cout << "To run model use: ./mark2 e <model_file>";
+    //     std::cout << std::endl;
+    //     return EXIT_FAILURE;
+    // }
+    // if (argc == 2) {
+    //     try {
+    //         size_t order = std::stoll(argv[1]);
+    //         mode1(order);
+    //     } catch (const std::invalid_argument &e) {
+    //         std::cerr << "OOPS! Order must be number" << e.what() << std::endl;
+    //     } catch (const std::out_of_range &e) {
+    //         std::cerr << "OOPS! Order out of range " << e.what() << std::endl;
+    //     } catch (...) {
+    //         std::cerr << "OOPS! unknown"<< std::endl;
+    //     }
+    // } else if (argc == 3) {
+    //     try {
+    //         size_t order = std::stoll(argv[1]);
+    //         mode1(order);
+    //     } catch (const std::invalid_argument &e) {
+    //         std::cerr << "OOPS! Order must be number" << e.what() << std::endl;
+    //     } catch (const std::out_of_range &e) {
+    //         std::cerr << "OOPS! Order out of range " << e.what() << std::endl;
+    //     } catch (...) {
+    //         std::cerr << "OOPS! unknown"<< std::endl;
+    //     }
+    // } else {
+    //     std::cout << "Invalid args!\n";
+    //     std::cout << "To build model use: ./mark2 <order>\n";
+    //     std::cout << "To run model use: ./mark2 <order> <model_file>";
+    //     std::cout << std::endl;
+    // }
+    // std::list<std::string> files = {
+    //     {"file1"},
+    //     {"file2"},
+    // };
+
+    // for (int i = 0; i != 100; ++i) {
+    //     PipeRead pipe;
+    //     std::string cmd =
+    //         "curl -s file:///home/marina/programming/git/mark2/case_003.txt";
+    //     auto res = pipe.read(cmd);
+    //     // std::cout << "out = " << res.first << std::endl;
+    //     std::cout << "ok  = " << res.second << std::endl;
+    // }
 
     // std::vector<int> v;
     // size_t n = 1024*1024*200; // 10 Mb
@@ -166,5 +392,5 @@ main()
     // // res = model.generate(State{{"one"}, {"fish"}}, 10);
     // std::cout << "RES3 : " << res << "\n";
 
-    return EXIT_SUCCESS;
+    // return EXIT_SUCCESS;
 }
