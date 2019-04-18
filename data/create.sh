113671a34648a2bbf7d36459cf400c923cd71f7a
ziva777 <ziva777@gmail.com>
2019-04-18 16:59:17 +0300

    Creating models script

--- data/create.sh
+++ data/create.sh
@@ -1,3 +1,10 @@
 #/usr/bin sh
 
-cat urls.txt | ./mark2 c 1 > model3.txt
+#cat urls3.txt | ./mark2 c 2 > model5.txt
+
+URL="file://"`pwd`
+
+awk -v a="$URL/" '{print a$0}' urls_00.txt | ./mark2 c 2 > model_00.txt
+cat urls_01.txt | ./mark2 c 2 > model_01.txt
+awk -v a="$URL/" '{print a$0}' urls_02.txt | ./mark2 c 2 > model_02.txt
+awk -v a="$URL/" '{print a$0}' urls_03.txt | ./mark2 c 2 > model_03.txt
--- data/rand_text.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin sh
-
-tr -dc A-Za-z0-9,.' '  < /dev/urandom | head -c 1048576
